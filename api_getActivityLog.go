276703b0fad11c378f5ac24b55d5af7f08aeeb08
Mark Nevill <mark.nevill@sensat.co.uk>
2023-06-06 17:36:23 +0100

    add GetCollectionActivityLog

--- api_getActivityLog.go
+++ api_getActivityLog.go
@@ -47,3 +47,44 @@ func (c *Client) GetActivityLog(spaceID string, startingAfterID string, navBlock
 	}
 	return &rsp, nil
 }
+
+type collectionID struct {
+	ID string `json:"id"`
+}
+
+// /api/v3/getActivityLog request
+type getCollectionActivityLogRequest struct {
+	SpaceID         string           `json:"spaceId"`
+	StartingAfterID string           `json:"startingAfterId,omitempty"`
+	NavigableBlock  navigableBlockID `json:"navigableBlock,omitempty"`
+	Collection      collectionID     `json:"collection,omitempty"`
+	Limit           int              `json:"limit"`
+}
+
+// GetActivityLog executes a raw API call /api/v3/getActivityLog.
+// If startingAfterId is "", starts at the most recent log entry.
+// navBlockID is the ID of a navigable block (like a page in a database)
+func (c *Client) GetCollectionActivityLog(spaceID string, startingAfterID string, navBlockID string, collID string, limit int) (*GetActivityLogResponse, error) {
+	req := &getCollectionActivityLogRequest{
+		SpaceID:         spaceID,
+		StartingAfterID: startingAfterID,
+		Limit:           limit,
+		NavigableBlock:  navigableBlockID{ID: navBlockID},
+		Collection:      collectionID{ID: collID},
+	}
+	var rsp GetActivityLogResponse
+	var err error
+	apiURL := "/api/v3/getActivityLog"
+	if err = c.doNotionAPI(apiURL, req, &rsp, &rsp.RawJSON); err != nil {
+		return nil, err
+	}
+	if err = ParseRecordMap(rsp.RecordMap); err != nil {
+		return nil, err
+	}
+	if len(rsp.ActivityIDs) > 0 {
+		rsp.NextID = rsp.ActivityIDs[len(rsp.ActivityIDs)-1]
+	} else {
+		rsp.NextID = ""
+	}
+	return &rsp, nil
+}
